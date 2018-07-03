05d161e9c3d70ee2f6867ab7e0d96ac46d354a1e
scx <sumitgood@icloud.com>
2018-07-04 00:10:51 +0100

    clean up

--- asm.s
+++ asm.s
@@ -2,43 +2,43 @@
 .export SIDSTEP 
 .export _SIDFILE
 .export _SIDPLAY
-_SIDFILE: .INCBIN "filename.sid",  $7e  
+_SIDFILE: .INCBIN "filename.sid",$7e
 
 SIDLOADPOS = $4000;
 SIDPLAYPOS = $4003;
 
 SIDSTEP: 
-    jsr SIDPLAYPOS
-    rts 
+	jsr SIDPLAYPOS
+	rts 
 
 _SIDINIT: 
-    lda #0 
-    tax 
-    tay 
-    jsr SIDLOADPOS
-    rts 
+	lda #0 
+	tax 
+	tay 
+	jsr SIDLOADPOS
+	rts 
 
 _SIDPLAY:
-           LDX #$00 
-           LDX #<IRQ
-           LDY #>IRQ
-           LDA #$00
-           STX $0314
-           STY $0315
-           STA $D012
-           LDA #$7F
-           STA $DC0D
-           LDA #$1B
-           STA $D011
-           LDA #$01
-           STA $D01A
-           CLI
-           RTS
+	LDX #<IRQ	;lo-byte
+	LDY #>IRQ	;hi-byte
+	STX $031	
+	STY $0315
+	LDA #$00
+	STA $D012
+	LDA #$7F
+	STA $DC0D
+	LDA #$1B
+	STA $D011
+	LDA #$01
+	STA $D01A
+	CLI
+	RTS
 
-IRQ:        INC $D019
-           LDA #$00
-           STA $D012
+IRQ:
+	INC $D019
+	LDA #$00
+	STA $D012	
 
-	   JSR SIDSTEP
-           JMP $EA7E
-
+	JSR SIDSTEP	;Step the SID
+	JMP $EA7E	;Back to Rom
+	
