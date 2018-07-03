31b3d3e88a31fe973e45e945f667b2f2832c8d62
scx <sumitgood@icloud.com>
2018-07-04 00:33:49 +0100

    fixed typo; more comments

--- asm.s
+++ asm.s
@@ -8,20 +8,20 @@ SIDLOADPOS = $4000;
 SIDPLAYPOS = $4003;
 
 SIDSTEP: 		;step SID
-	jsr SIDPLAYPOS
+	jsr SIDPLAYPOS  ;jump to SIDPLAYPOS, return
 	rts 
 
 _SIDINIT:		;init SID 
-	lda #0 
-	tax 
-	tay 
-	jsr SIDLOADPOS
+	lda #0
+	tax		;transfer accumilator to x register
+	tay		;transfer accumilator to y register
+	jsr SIDLOADPOS ;jump to SIDLOADPOS, return
 	rts 
 
 _SIDPLAY:
 	LDX #<IRQ	;lo-byte
 	LDY #>IRQ	;ihi-byte
-	STX $031	
+	STX $0314
 	STY $0315
 
 	LDA #$00	;raster pos
