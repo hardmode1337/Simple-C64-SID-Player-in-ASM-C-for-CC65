5954d979b866e73e6e7451279f6e3ad712278428
scx <sumitgood@icloud.com>
2018-07-03 21:23:36 +0100

    no message

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 #include "main.h"
 #include "vblank.h"
 
-unsigned int SIDSIZE = (1024)*8; 			//8mb should be more than enough
+unsigned int SIDSIZE = (1024)*8; 			//8kb should be more than enough
 unsigned int SIDLOAD = 0x4000;				//Make sure you offset your sid at $4000 using sidereloc -p 40
 
 int main(void)
